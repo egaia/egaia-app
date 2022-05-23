3514409dd09f4265721e61561c4068efb35494e7
guillaume-ventura <venturaguillaume01@gmail.com>
2022-05-23 21:07:14 +0200

    ✨ Add user location on map

--- screens/CollectPointsScreen.tsx
+++ screens/CollectPointsScreen.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View, Image} from 'react-native';
 
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import EgaiaContainer from "../components/EgaiaContainer";
@@ -9,35 +9,56 @@ import {LoaderContext} from "../contexts/loader";
 import {getAllCollectPoints} from "../repositories/collect_point_repository";
 import {CollectPoint} from "../models/CollectPoint";
 import {Colors} from "../services/constants";
+import * as Location from 'expo-location';
 
 export default function CollectPointsScreen({navigation}: NativeStackScreenProps<any>) {
 
     const { setLoading } = useContext<LoaderContextType>(LoaderContext)
 
+    const [location, setLocation] = useState<Location.LocationObject | undefined>(undefined);
+    const [errorMsg, setErrorMsg] = useState<string | undefined>(undefined);
+
     const [collectPoints, setCollectPoints] = useState<CollectPoint[]>([])
     const [selectedCollectPoint, setSelectedCollectPoint] = useState<CollectPoint|undefined>(undefined)
 
-    const initialRegion: Region = {
-        latitude: 45.764043,
-        longitude: 4.835659,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    }
+    const [region, setRegion] = useState<Region>({
+        latitude: 45.782443169021704,
+        longitude: 4.889184942744952,
+        latitudeDelta: 0.06,
+        longitudeDelta: 0.06,
+    })
 
     useEffect(() => {
-        setLoading(true)
+        (async () => {
+            setLoading(true)
 
-        const getData = async () => await getAllCollectPoints(initialRegion.latitude, initialRegion.longitude).then(results => {
-            if(typeof results !== 'string') {
-                setCollectPoints(results)
-            } else {
-                console.error(results)
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
             }
-        }).catch(error => {
-            console.error(error.message)
-        })
 
-        getData().then(() => setLoading(false))
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            setRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.05,
+                longitudeDelta: 0.05
+            })
+        })().then(() => {
+            const getData = async () => await getAllCollectPoints(region.latitude, region.longitude).then(results => {
+                if(typeof results !== 'string') {
+                    setCollectPoints(results)
+                } else {
+                    console.error(results)
+                }
+            }).catch(error => {
+                console.error(error.message)
+            })
+
+            getData().then(() => setLoading(false))
+        })
     }, [])
 
     const refreshMarkers = (value: Region) => {
@@ -51,7 +72,7 @@ export default function CollectPointsScreen({navigation}: NativeStackScreenProps
             console.error(error.message)
         })
 
-        getData().then()
+        getData().then(() => setRegion(value))
     }
 
     return (
@@ -59,11 +80,15 @@ export default function CollectPointsScreen({navigation}: NativeStackScreenProps
             <View style={styles.container}>
                 <MapView
                     style={{width: '100%', height: '100%'}}
-                    initialRegion={initialRegion}
-                    minZoomLevel={12}
+                    region={region}
                     onRegionChangeComplete={(value) => refreshMarkers(value)}
                     onMarkerDeselect={() => setSelectedCollectPoint(undefined)}
                 >
+                    {location &&
+                      <Marker coordinate={location.coords}>
+                        <View style={styles.userLocation}/>
+                      </Marker>
+                    }
                     {collectPoints.map(collectPoint => {
                         return (
                             <Marker
@@ -75,6 +100,20 @@ export default function CollectPointsScreen({navigation}: NativeStackScreenProps
                         )
                     })}
                 </MapView>
+                {
+                    location &&
+                  <TouchableOpacity style={styles.locationButton} onPress={() => {
+                      console.log('pressed', location)
+                      location && setRegion({
+                          latitude: location.coords.latitude,
+                          longitude: location.coords.longitude,
+                          latitudeDelta: 0.05,
+                          longitudeDelta: 0.05
+                      })
+                  }}>
+                    <Image style={styles.buttonIcon} source={require("../assets/icons/target.png")}/>
+                  </TouchableOpacity>
+                }
                 {selectedCollectPoint !== undefined ?
                     <View style={styles.addressGlobalContainer}>
                         <View style={styles.trashCansContainer}>
@@ -107,6 +146,17 @@ const styles = StyleSheet.create({
         height: 1,
         width: '80%',
     },
+    userLocation: {
+        height: 20,
+        width: 20,
+        backgroundColor: '#0079fd',
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 50,
+        shadowColor: '#0079fd',
+        shadowOpacity: 1,
+        shadowRadius: 10,
+    },
     addressGlobalContainer: {
         height: '20%',
         width: '85%',
@@ -128,5 +178,18 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.secondary,
         borderBottomLeftRadius: 15,
         borderBottomRightRadius: 15,
-    }
+    },
+    buttonIcon: {
+        width: 30,
+        height: 30,
+        tintColor: Colors.white
+    },
+    locationButton: {
+        position: "absolute",
+        padding: 15,
+        top: 20,
+        right: 20,
+        backgroundColor: Colors.primary,
+        borderRadius: 50
+    },
 });
