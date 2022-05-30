8b86099a891ea5abbce17fb2b3bef08e4d8ba8bf
guillaume-ventura <venturaguillaume01@gmail.com>
2022-05-30 19:30:40 +0200

    💄 Add style ChallengeScreen

--- screens/challenges/ChallengeScreen.tsx
+++ screens/challenges/ChallengeScreen.tsx
@@ -9,7 +9,7 @@ import {UserContextType} from "../../services/types";
 
 const ChallengeScreen = ({navigation, route}: NativeStackScreenProps<any>) => {
 
-    const { user } = useContext<UserContextType>(UserContext)
+    const {user} = useContext<UserContextType>(UserContext)
 
     const challenge: Challenge = route.params?.challenge
 
@@ -21,31 +21,36 @@ const ChallengeScreen = ({navigation, route}: NativeStackScreenProps<any>) => {
         <EgaiaContainer>
             <View style={styles.globalContainer}>
                 <View style={styles.titleContainer}>
-                    <Text>Défi de la semaine</Text>
+                    <Text style={styles.titleText}>Défi de la semaine</Text>
                 </View>
                 <View style={styles.challengeContainer}>
                     <View>
                         <Text style={styles.challengeTitle}>{challenge.title}</Text>
-                        <Text style={styles.description}>Pour réussir ton défi, prends une photo et gagne des gaïas</Text>
+                        <Text style={styles.description}>Pour réussir ton défi, prends une photo et gagne des
+                            gaïas.</Text>
                     </View>
-                    {
-                        user !== undefined ?
-                        challenge.participation ?
-                            <View>
-                                <Image resizeMode="center" style={{width: 300, height: 300}} source={{uri: challenge.participation.picture}} />
-                                <Text>Tu as participé à ce défi</Text>
-                                {challenge.participation.valid ?
-                                    <Text>Celui-ci a été validé félicitations</Text>
-                                    : <Text>Celui-ci est en attente de validation</Text>}
-                            </View>
-                            :
-                            <TouchableOpacity style={styles.button} onPress={clickOnParticipate}>
-                                <Text>Réaliser le défi</Text>
-                                <Text>{'-->'}</Text>
-                            </TouchableOpacity>
-                            :
-                            <Text>Connectez-vous pour participer à ce défi</Text>
-                    }
+                    <View style={styles.infoContainer}>
+                        {
+                            user !== undefined ?
+                                challenge.participation ?
+                                    <View style={styles.imageContainer}>
+                                        <Image style={styles.image} resizeMode="cover"
+                                               source={{uri: challenge.participation.picture}}/>
+                                        <Text style={styles.participationText}>Tu as participé à ce défi</Text>
+                                        {challenge.participation.valid ?
+                                            <Text style={styles.validText}>Celui-ci a été validé félicitations !</Text>
+                                            : <Text style={styles.waitText}>Celui-ci est en attente de validation</Text>}
+                                    </View>
+                                    :
+                                    <TouchableOpacity style={styles.button} onPress={clickOnParticipate}>
+                                        <Text style={styles.buttonText}>Réaliser le défi</Text>
+                                        <Image resizeMode="contain" style={styles.arrow}
+                                               source={require("../../assets/img/right-arrow.png")}/>
+                                    </TouchableOpacity>
+                                :
+                                <Text>Connectez-vous pour participer à ce défi</Text>
+                        }
+                    </View>
                 </View>
             </View>
         </EgaiaContainer>
@@ -65,33 +70,85 @@ const styles = StyleSheet.create({
         alignItems: "flex-start",
         paddingLeft: 20
     },
+    titleText: {
+        fontSize: 24,
+        fontWeight: "800"
+    },
     challengeContainer: {
         width: '100%',
         height: '85%',
         backgroundColor: Colors.primary,
         borderTopRightRadius: 25,
         borderTopLeftRadius: 25,
-        justifyContent: "space-between",
+        justifyContent: "flex-start",
         alignItems: "flex-start",
         paddingVertical: 50,
         paddingHorizontal: 30
     },
     challengeTitle: {
-        fontSize: 20,
-        fontWeight: "bold",
-        color: Colors.white
+        fontSize: 24,
+        fontWeight: "800",
+        color: Colors.white,
+        marginBottom: 5
     },
     description: {
         fontSize: 16,
         color: Colors.white
     },
+    infoContainer: {
+        height: '100%',
+        width: '100%',
+        marginTop: 50
+    },
+    imageContainer: {
+        width: '100%',
+        height: 250,
+        borderRadius: 15,
+        borderWidth: 2,
+        borderColor: Colors.white,
+    },
+    participationText: {
+        fontSize: 16,
+        color: Colors.white,
+        textAlign: "center",
+        marginTop: 20
+    },
+    waitText: {
+        fontSize: 16,
+        color: Colors.white,
+        textAlign: "center",
+        marginTop: 10
+    },
+    validText: {
+        fontSize: 18,
+        color: Colors.white,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginTop: 20
+    },
+    image: {
+        height: '100%',
+        width: '100%',
+        borderRadius: 15,
+    },
     button: {
         width: "60%",
         flexDirection: "row",
-        justifyContent: "space-evenly",
+        justifyContent: "space-between",
         alignItems: "center",
-        padding: 20,
-        backgroundColor: Colors.white
+        paddingVertical: 20,
+        paddingHorizontal: 30,
+        backgroundColor: Colors.white,
+        position: "absolute",
+        bottom: 150
+    },
+    buttonText: {
+        fontSize: 16
+    },
+    arrow: {
+        width: 20,
+        height: 20,
+        tintColor: Colors.black
     }
 })
 
