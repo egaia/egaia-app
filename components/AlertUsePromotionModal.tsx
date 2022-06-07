cd2c271bc619f6e984b6fd2b082be76f278cb28d
guillaume-ventura <venturaguillaume01@gmail.com>
2022-06-07 10:58:37 +0200

    💄 Update AlertUsePromotionModal style

--- components/AlertUsePromotionModal.tsx
+++ components/AlertUsePromotionModal.tsx
@@ -2,31 +2,33 @@ import {Modal, StyleSheet, Text, TouchableOpacity, View, Image} from "react-nati
 import {Colors} from "../services/constants";
 import {Promotion} from "../models/Promotion";
 
-interface PlaceModalProps {
+interface AlertUsePromotionModalProps {
     promotion: Promotion,
     visible: boolean,
     onClose: () => void,
     onPressYes: () => void
 }
 
-const PlaceModal = ({promotion, visible, onClose, onPressYes}: PlaceModalProps) => {
+const AlertUsePromotionModal = ({promotion, visible, onClose, onPressYes}: AlertUsePromotionModalProps) => {
     return (
         <Modal animationType="fade" visible={visible} transparent={true}>
             <View style={styles.modalContainer}>
                 <View style={styles.globalContainer}>
-                    <TouchableOpacity style={styles.close} onPress={onClose}>
+                    {/*<TouchableOpacity style={styles.close} onPress={onClose}>
                         <Image style={styles.icon} source={require("../assets/icons/cross.png")}/>
-                    </TouchableOpacity>
+                    </TouchableOpacity>*/}
                     <View style={styles.bodyContainer}>
-                        <Text>Voulez-vous profitez de cette offre ?</Text>
-                        <Text>{promotion.partner?.name} : {promotion.label}</Text>
-                        <Text>Vous utiliserez {promotion.cost} de vos gaïas</Text>
-                        <TouchableOpacity onPress={onClose}>
-                            <Text style={styles.btn}>Annuler</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={onPressYes}>
-                            <Text style={styles.btn}>Utiliser l'offre</Text>
-                        </TouchableOpacity>
+                        <Text style={styles.title}>Voulez-vous profitez de cette offre ?</Text>
+                        <Text style={{color: Colors.white}}>{promotion.partner?.name} : {promotion.label}</Text>
+                        <Text style={{color: Colors.white}}>Vous utiliserez {promotion.cost} de vos gaïas</Text>
+                        <View style={styles.btnContainer}>
+                            <TouchableOpacity onPress={onClose}>
+                                <Text style={styles.btn}>Annuler</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={onPressYes}>
+                                <Text style={styles.btn}>Utiliser l'offre</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
             </View>
@@ -40,13 +42,15 @@ const styles = StyleSheet.create({
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "rgba(0,0,0,0.6)"
     },
 
     globalContainer: {
         backgroundColor: Colors.background,
-        width: '90%',
+        width: '80%',
         height: '30%',
-        borderRadius: 15
+        borderRadius: 15,
+        justifyContent: "center"
     },
 
     close: {
@@ -71,14 +75,33 @@ const styles = StyleSheet.create({
         tintColor: Colors.white
     },
 
+    title: {
+        fontSize: 16,
+        fontWeight: "600",
+        marginBottom: 20,
+        color: Colors.white
+    },
+
+    btnContainer: {
+        width: '100%',
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        marginTop: 20
+    },
+
     btn: {
+        width: 115,
         borderWidth: 1,
         borderRadius: 5,
         paddingHorizontal: 10,
         paddingVertical: 5,
         marginTop: 5,
-        fontWeight: "500"
+        fontWeight: "500",
+        textAlign: "center",
+        color: Colors.white,
+        borderColor: Colors.white
     },
 })
 
-export default PlaceModal
+export default AlertUsePromotionModal
